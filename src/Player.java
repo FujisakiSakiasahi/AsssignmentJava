0e78cd567f19ff50b2f7b321f3b8c420ff6f640d
Jhun Kit <jhunkit2753@gmail.com>
2022-02-11 03:07:34 +0800

    created Player.java class

---
+++ src/Player.java
@@ -0,0 +1,41 @@
+public class Player{
+    private int id;
+    private String name;
+    private int points;
+    private int num = 0;
+    
+    //constructor
+    public Player(String name, int points){
+        this.id = 100 + num;
+        this.name = name;
+        this.points = points;
+        num++;
+    }
+
+    //setters
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setPoints(int points){
+        this.points = points;
+    }
+
+    //getters
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int points(){
+        return points;
+    }
+
+    //display
+    public String toString(){
+        return ("\nPlayer ID: " + id + "\nPlayer Name: " + name + "\nPoints: " + points); 
+    }
+}
